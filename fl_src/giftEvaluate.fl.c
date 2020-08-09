a2d12f9301b757f1737590cb0a82746cce63bf95
cornerStone-Dev <42221661+cornerStone-Dev@users.noreply.github.com>
2020-08-09 15:46:23 -0400

    implemented the type checking add procedure. Going to implement the rest of primitive procedures in a similiar fashion, in the pattern of apply.

--- fl_src/giftEvaluate.fl.c
+++ fl_src/giftEvaluate.fl.c
@@ -6,8 +6,9 @@ giftEvaluate(S_Environment $e, u8 $cursor)
 	T_hashTableNode $result;
 	u8              $symbol;
 	u8              $value;
+	u8              $$procedure;
 	u64             length;
-	u64 argumentCount = 0;
+	u64 stackCount = 0;
 
 evaluateDispatch:
 	switch($cursor)
@@ -56,20 +57,12 @@ evaluateDispatch:
 		default:
 		printf("Syntax error on ");
 		giftPrint(cursor);
-		printf(" which does not a valid start to an expression.\n");
+		printf(" which is an invalid start to an expression.\n");
 		return @e.undefinedValue;
 	}
 	
 	switch($cursor)
 	{
-
-		//~ case LIST_SYMBOL:
-		//~ value+=1;
-		//~ length = strlen(value)+1; // including null
-		//~ printf("%s", value);
-		//~ value+=length;
-		//~ goto loop;
-		
 		// TODO pull out define and set! into function
 		case LIST_DEFINE:{
 		// define is a special form
@@ -235,36 +228,78 @@ evaluateDispatch:
 			goto evaluateDispatch;
 		}
 		
+		// other procedure
+		case LIST_START:
+		case LIST_SYMBOL:
+		value = getHeapCursor(e);
+		do{
+			// accumulate procedure and arguments
+			$e.sp = giftEvaluate(e, cursor);
+			e.sp+=1;
+			stackCount+=1;
+			cursor = skipItem(cursor);
+		} while($cursor != LIST_END);
+		break;
+		
+		// primitive procedure
 		case LIST_PLUS:
 		cursor+=1;
+		// open a heap cursor
 		value = getHeapCursor(e);
+		// record start of args
+		procedure = e.sp;
 		while($cursor != LIST_END)
 		{
 			// accumulate arguments
 			$e.sp = giftEvaluate(e, cursor);
 			e.sp+=1;
-			argumentCount+=1;
+			stackCount+=1;
 			cursor = skipItem(cursor);
 		}
-		s64 total = 0;
+		// e.sp-=stackCount;
+		// return giftAddProcedure(e, value, procedure, stackCount);
+		value = giftAddProcedure(e, value, procedure, stackCount);
 		// pop stack
-		e.sp-=argumentCount;
-		for(u64 x=0; x < argumentCount; x+=1)
+		e.sp-=stackCount;
+		u8 $transformedSource=value-4;
+		for(u32 x =0; x<20;x+=1)
+		{
+			printf("[%02X]",transformedSource[x]);
+		}
+		printf("\n");
+		printf("heapIndex=%ld\n",e.heapIndex);
+		printf("heapTop=%ld\n",e.heapTop);
+		printf("heapBottom=%ld\n",e.heapBottom);
+		return value;
+		
+		default:
+		printf("Syntax error on ");
+		giftPrint(cursor);
+		printf(" which is not a valid procedure.\n");
+		return @e.undefinedValue;
+	}
+	
+	// get bottom of stack
+	procedure = e.sp-stackCount;
+	// apply
+	switch($($procedure))
+	{
+		case LIST_PLUS:
+		// skip first procedure
+		procedure+=1;
+		s64 total = 0;
+		
+		for(u64 x=0; x < stackCount; x+=1)
 		{
 			total += readListInt(
 				(e.sp[x])+1,
 				$(e.sp[x]));
 		}
-		//~ while(argumentCount){
-			//~ total += readListInt(
-				//~ ($(e.sp-argumentCount))+1,
-				//~ $($(e.sp-argumentCount)));
-			//~ argumentCount-=1;
-			
-		//~ }
 		
 		cursor = listWriteInt(value, total);
 		finalizeHeapCursor(e, value, cursor);
+		// pop stack
+		e.sp-=stackCount;
 		u8 $transformedSource=value-4;
 		for(u32 x =0; x<20;x+=1)
 		{
@@ -299,9 +334,81 @@ evaluateDispatch:
 		//~ printf("unknown value");
 		//~ value+=1;
 		//~ goto loop;
+		
 	}
 	
 	
 	return 0;
+}
+
+u8$
+giftAddProcedure(S_Environment $e, u8 $cursor, u8 $$args, u64 numArgs)
+{
+	u8 $end;
+	u8 $currentArg;
+	U_Data arg1, arg2;
+	u8 type1, type2, typeComp;
+	s64 total = 0;
+	if(numArgs<2){
+		printf("ERROR: minimum of 2 parameters for '+' procedure.\n");
+	}
+	currentArg = $args;
+	if($currentArg == LIST_FLOAT) // its a float
+	{
+		currentArg+=1;
+		arg1.d = readListFloat(currentArg);
+		type1 = 2;
+	} else if ($currentArg <= LIST_INT8)
+	{ // its an int
+		currentArg+=1;
+		arg1.i = readListInt(currentArg, ($(currentArg-1)));
+		type1 = 1;
+	} else {
+		printf("ERROR: Math is for only floats and ints.\n");
+		return @e.undefinedValue;
+	}
+	numArgs-=1;
+	
+	do{
+		args+=1;
+		currentArg = $args;
+		if($currentArg == LIST_FLOAT) // its a float
+		{
+			currentArg+=1;
+			arg2.d = readListFloat(currentArg);
+			type2 = 2;
+		} else if ($currentArg <= LIST_INT8)
+		{ // its an int
+			currentArg+=1;
+			arg2.i = readListInt(currentArg, ($(currentArg-1)));
+			type2 = 1;
+		} else {
+			printf("ERROR: Math is for only floats and ints.\n");
+			return @e.undefinedValue;
+		}
+		
+		typeComp = type1|type2;
+		if (typeComp==1)
+		{
+			arg1.i = arg1.i+arg2.i;
+		} else if (typeComp==2) {
+			arg1.d = arg1.d+arg2.d;
+		} else if (type1==2) {
+			arg1.d = arg1.d+arg2.i;
+		} else {
+			arg1.d = arg1.i+arg2.d;
+			type1 = 2;
+		}
+		numArgs-=1;
+	} while(numArgs);
+	
+	// we have finished the computation
+	if(type1 == 1){
+		end = listWriteInt(cursor, arg1.i);
+	} else {
+		end = listWriteFloat(cursor, arg1.d);
+	}
+	finalizeHeapCursor(e, cursor, end);
+	return cursor; 
 }
 
